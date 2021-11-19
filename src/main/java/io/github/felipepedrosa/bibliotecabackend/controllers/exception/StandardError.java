cdf21e19ce40e65b222b78478eac609d75ce0481
felipepedrosa <felipeepedrosa@gmail.com>
2021-11-19 11:50:07 -0300

    Added default exception handler
    Created ApplicationExceptionHandler and Standard.

---
+++ src/main/java/io/github/felipepedrosa/bibliotecabackend/controllers/exception/StandardError.java
@@ -0,0 +1,56 @@
+package io.github.felipepedrosa.bibliotecabackend.controllers.exception;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.List;
+
+public class StandardError implements Serializable {
+    private Instant timestamp;
+    private Integer httpStatus;
+    private String path;
+    private String description;
+    private List<String> messages;
+
+    public StandardError() {
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(Integer httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
+}
